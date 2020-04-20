75175f8d4e3ea5b3802cf66793523dd6fec616e3
Chris <chrismueller3@hotmail.com>
2020-04-20 19:06:35 -0400

    First bit of the game

--- src/Board.java
+++ src/Board.java
@@ -3,6 +3,10 @@ import java.util.Arrays;
 
 public class Board {
 
+    /**
+     * The board variable itself.
+     * The first index is the x axis, while the second is the y axis. Assumes 0,0 is at the bottom left corner.
+     */
     private int[][] board=new int[3][3];
 
     public Board() {
@@ -16,8 +20,59 @@ public class Board {
         if (board[x][y]!=0) {
             return false;
         }
+        int XO;
+        if (c=='x'||c=='X') {
+            XO=1;
+        } else if (c=='o'||c=='O') {
+            XO=4;
+        } else {
+            return false;
+        }
+        board[x][y]=XO;
 
         return true;
+    }
+
+    /**
+     * Checks the board to see if anybody has won that game.
+     * Displays the winning board
+     * @return Returns false if there isn't a winner, true if there is a winner
+     */
+    public boolean checkBoard() {
+
+        return true;
+    }
+
+    /**
+     * Displays the current board.
+     */
+    public void displayBoard() {
+        for (int j=2;j>=0;j--) {
+            for (int i=0;i<3;i++) {
+                System.out.print(this.getBoardValue(i,j)+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Returns a letter representation of an index.
+     * Mainly used in the displayBoard method right now.
+     * Saves a lot of space in the displayBoard method.
+     * @param x The x index one wants
+     * @param y The y index one wants
+     * @return The letter representation of the index in question
+     */
+    private char getBoardValue(int x, int y) {
+        if (board[x][y]==1) {
+            return 'X';
+        } else if (board[x][y]==4) {
+            return 'O';
+        } else if (board[x][y]==0) {
+            return '-';
+        } else {
+            return 'e';
+        }
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Start of a Tic Tac Toe Command Line Game");
-
+        Board board=new Board();
+        board.setPosition(0,2,'X');
+        board.setPosition(0,2,'o');
+        board.setPosition(2,0,'o');
+        board.displayBoard();
 
     }
 }
